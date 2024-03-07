7906b96a908129268f0e11e67afed2ed67e456da
Hamid <gmhamid6@gmail.com>
2024-03-07 09:56:19 +0330

    add scoped in Progeam

--- GiveTurn.Blazor/Program.cs
+++ GiveTurn.Blazor/Program.cs
@@ -1,10 +1,15 @@
 using GiveTurn.Blazor;
+using GiveTurn.Blazor.Services;
+using GiveTurn.Blazor.Services.Interfaces;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<ITurnServices, TurnServices>();
+builder.Services.AddScoped<IUserServices, UserServices>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7251/") });
 
