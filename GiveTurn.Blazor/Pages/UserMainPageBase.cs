1205f7e09c2a72142ebb316c0c8d82b11fac16e4
Hamid <gmhamid6@gmail.com>
2024-03-08 09:57:26 +0330

    Home page ui wroted

---
+++ GiveTurn.Blazor/Pages/UserMainPageBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GiveTurn.Blazor.Pages
+{
+    public class UserMainPageBase : ComponentBase
+    {
+        [Parameter]
+        public string Username { get; set; }
+        [Parameter]
+        public string Password { get; set; }
+    }
+}
